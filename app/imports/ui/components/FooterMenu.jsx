37968378bc41631ba0640d53a25ccca74dcaeb2e
Mikhail Shkaralevich <mkaralevich8@gmail.com>
2023-10-18 14:49:15 -1000

    Done

--- app/imports/ui/components/FooterMenu.jsx
+++ app/imports/ui/components/FooterMenu.jsx
@@ -5,25 +5,25 @@ import { Container, Row, Col, Image } from 'react-bootstrap';
 const FooterMenu = () => (
   <Container className="footer" fluid>
     <Row className="justify-content-center">
-      <Col className="col-4 col-md-offset-2" id="contact">
+      <Col className="col-4 text-center" id="contact">
         CONTACT US
         <div className="mt-3">P 808.845.8044</div>
         <div>© 2023 All Rights</div>
         <div>Reserved | site by</div>
         <div style={{ color: '#888888' }}>inception designs</div>
       </Col>
-      <Col className="col-4">
+      <Col className="col-4 text-center">
         ADDRESS
         <div className="mt-3">Helenas Hawaiian Food</div>
         <div>1240 N. School St.</div>
       </Col>
       <Col className="col-4">
-        MEDIA
+        <div className="media" id="media">MEDIA</div>
         <Row className="no-gutters">
           <Col className="col-3">
             <Image src="images/yelp.png" className="media-icon" fluid alt="img" />
           </Col>
-          <Col className="col-3">
+          <Col className="col-3 ps-0">
             <Image src="images/square-instagram.png" className="media-icon" fluid alt="img" />
           </Col>
         </Row>
--- app/imports/ui/components/MiddleMenu.jsx
+++ app/imports/ui/components/MiddleMenu.jsx
@@ -17,6 +17,7 @@ const MiddleMenu = () => (
       </Col>
       <Col className="col-4">
         <p>In 1946, Helen Chock opened the doors to Helenas Hawaiian Food, a humble eatery on North King St. in Honolulu, Hawaii.</p>
+        {/* eslint-disable-next-line max-len */}
         <p>Over the past 64 years Helenas Hawaiian Food has been serving the local community quality Hawaiian cuisine. In 2000, the James Beard Foundation awarded Helenas Hawaiian Food the Regional Classic Award. Helenas is one of a handful of restaurants in Hawaii to receive this distinctive award. After nearly 55 years, the restaurant was relocated to its current location on 1240 N. School Street.
         </p>
         <p>Helenas remains the simple and unpretentious eatery it was over 6 decades ago. The restaurant is now run by Helens grandson, Craig Katsuyoshi.</p>
@@ -24,7 +25,7 @@ const MiddleMenu = () => (
     </Row>
     <Row className="row" id="fake-row">
       <Col className="col">
-        <span className="my-5" id="fake-span"></span>
+        <span className="my-5" id="fake-span" />
       </Col>
     </Row>
     <Row className="row mt-5 justify-content-center" id="row2">
